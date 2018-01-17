6b43f2f5daf2a4d65ea25848e24ca79ad146ebec
chabby <503589899@qq.com>
2018-01-17 18:07:38 +0800

    登录验证

--- resources/views/admin/admin/login.php
+++ resources/views/admin/admin/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="zh-cn" xmlns="http://www.w3.org/1999/html">
 <head>
-    <title>Matrix Admin</title><meta charset="UTF-8" />
+    <title>安心充后台管理系统</title><meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/css/bootstrap.min.css" />
     <link rel="stylesheet" href="/css/bootstrap-responsive.min.css" />
--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Matrix Admin</title>
+    <title>安心充后台管理系统</title>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" href="/css/bootstrap.min.css"/>
