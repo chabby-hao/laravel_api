86ff2495721547687f4362b6c9f5482e07dfbd17
chabby <503589899@qq.com>
2018-03-16 15:36:53 +0800

    弹框

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -21,6 +21,7 @@ class ActivityController extends Controller
     {
         $output = [
             'open'=>0,
+            'show_modal'=>0,//首页是否展示弹框
         ];
         if(ActivityService::isOpenPaySendActivity()){
             $output['open'] = 1;
