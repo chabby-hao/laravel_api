bc30d46e6f632b661c1f0458f27774df869176c1
chabby <503589899@qq.com>
2018-05-10 16:45:47 +0800

    换电

--- app/Models/Battery.php
+++ app/Models/Battery.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Models\Battery
  *
- * @property int $id
  * @property string|null $battery_id
  * @property int|null $udid
  * @property int|null $status 0=不可用，1=可用，2=使用中，3=维护中
@@ -15,7 +14,6 @@ use Illuminate\Database\Eloquent\Model;
  * @property \Carbon\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Battery whereBatteryId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Battery whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Battery whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Battery whereStatus($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Battery whereUdid($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Battery whereUpdatedAt($value)
@@ -29,5 +27,7 @@ class Battery extends Model
     protected $guarded = [];
 
     protected $table = 'battery';
+
+    protected $primaryKey = 'battery_id';
 
 }
