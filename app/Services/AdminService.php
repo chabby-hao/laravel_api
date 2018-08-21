2023835c308bb8548ac37dbfbd4177445e10134b
chabby <chabby@vipcare.com>
2018-08-21 19:50:58 +0800

    test

--- app/Services/AdminService.php
+++ app/Services/AdminService.php
@@ -80,6 +80,7 @@ class AdminService extends BaseService
     public static function getDeviceNos($isInt = false)
     {
         $config = session()->get('user_config');
+        var_dump($config);
         if($config) {
             $config = json_decode($config, true);
             $data = $config['device_nos'] ?: [];
