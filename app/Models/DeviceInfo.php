675d57a6082da24c5f99adc69b653c6ddb971442
chabby <chabby@vipcare.com>
2018-08-21 20:02:33 +0800

    test

--- app/Models/DeviceInfo.php
+++ app/Models/DeviceInfo.php
@@ -40,7 +40,7 @@ class DeviceInfo extends Model
     public static function getAllDeviceNo($whereIn = [])
     {
 var_dump($whereIn);
-        $model = self::where([]);
+        $model = new self();
         if($whereIn){
             var_dump($whereIn);
             $model->whereIn('device_no', $whereIn);
