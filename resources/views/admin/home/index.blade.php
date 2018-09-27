9fb52ba68e07c2dcbc6a3e5d30f30937b5b104fa
chabby <chabby@vipcare.com>
2018-09-27 15:32:34 +0800

    设备统计

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -3,7 +3,7 @@
         <div id="content-header">
             <div id="breadcrumb"><a href="#" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a>
                 <a href="#" class="current">图表统计</a></div>
-            <h1>图标统计</h1>
+            <h1>图表统计</h1>
         </div>
         <div class="container-fluid">
 
