9a9abc0ab0fe0c69d81fe2afa11a2109a175fdec
Eme <eme@Legend.local>
2018-09-29 15:41:43 +0800

    安心充

--- resources/views/admin/home/monthdetail.blade.php
+++ resources/views/admin/home/monthdetail.blade.php
@@ -146,7 +146,7 @@
 			        		item+='<tr><td>'+result.date+'</td><td>'+result.charge_times+'</td><td>'+result.electric_quantity+'</td><td>'+result.charge_duration+'</td><td>'+result.user_cost_amount+'</td><td>'+result.user_count+'</td><td>'+result.shared_amount+'</td></tr>';
 			        });
 			        $('.myTbody').append(item);
-			        lastPage=res.data.lastPage;
+			        lastPage=res.lastPage;
 			        $('.big_bg').css({'display':'none'});
       			}
       		},
