1161f70563a80d7c4b8803615737e9f7c546132f
chabby <chabby@vipcare.com>
2018-09-29 11:41:49 +0800

    设备统计

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -169,6 +169,8 @@ class HomeController extends BaseController
             ->selectRaw('substr(date,1,7) as date,device_no,sum(shared_amount) as shared_amount, sum(device_cost_amount) as device_cost_amount, sum(user_cost_amount) as user_cost_amount, sum(charge_times) as charge_times, sum(electric_quantity) as electric_quantity, sum(charge_duration) as charge_duration, sum(user_count) as user_count')
             ->paginate();
 
+        dd($devices);
+
         $datas = $devices->items();
 
         /** @var DeviceCostDetail $data */
